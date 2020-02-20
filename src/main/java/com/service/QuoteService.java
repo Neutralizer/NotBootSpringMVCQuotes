df1c9006fba562ad3d99b799eeff7506e24ffd6e
Tsvetan Trifonov <tsmtr_vd@abv.bg>
2020-02-19 23:29:12 -0800

    all methods integrated with data-jpa

--- src/main/java/com/service/QuoteService.java
+++ src/main/java/com/service/QuoteService.java
@@ -1,85 +1,106 @@
 package com.service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import com.dao.QuoteRepo;
 import com.dao.QuoteRepository;
+import com.model.Quote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.dao.QuoteRepo;
-import com.model.Quote;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 
 /**
- * 
  * QuoteService class for accessing the dao layer.
- * 
- * @author Neutralizer
  *
+ * @author Neutralizer
  */
 @Service
 public class QuoteService {
 
-	@Autowired
+    @Autowired
     private QuoteRepo quoteRepository;
 
     @Autowired
     private QuoteRepository quoteRepository1;
 
 
-	/**
-	 * Return all quotes from repo.
-	 * @return All quotes
-	 */
+    /**
+     * Return all quotes from repo.
+     *
+     * @return All quotes
+     */
     public List<Quote> getAllQuotes() {
 //        return quoteRepository.getAllQuotes();
-        List<Quote> all = (List<Quote>) quoteRepository1.findAll();
-        return all;
+        return (List<Quote>) quoteRepository1.findAll();
     }
 
     /**
-     * 
      * Retrieves quote by its id. Not zero based.
+     *
      * @param id The id of the quote
      * @return The quote
      */
     public Quote getQuote(int id) {
-        if (id < 1) throw new IllegalArgumentException("index can't be 0 or negative");
-        return quoteRepository.get(id -1);
+//        if (id < 1) throw new IllegalArgumentException("index can't be 0 or negative");
+//        return quoteRepository.get(id -1);
+        Optional<Quote> optional = quoteRepository1.findById(id);
+        return optional.orElse(new Quote()); // TODO null or empty ?
     }
 
     /**
      * Adds quote to repo.
+     *
      * @param quote The quote to be added
      */
     public void addQuote(Quote quote) {
-    	quoteRepository.add(quote);
+//    	quoteRepository.add(quote);
+        quoteRepository1.save(quote);
     }
 
     /**
      * Removes quote by id.
+     *
      * @param id The id of quote to be removed
      */
     public void remove(int id) {
-    	quoteRepository.remove(id);
+//    	quoteRepository.remove(id);
+        quoteRepository1.deleteById(id);
     }
 
     /**
      * Adds 1 rating to the quote
+     *
      * @param id The id of the quote
      */
-    public void addRating(int id){
-        quoteRepository.addRating(id);
+    public void addRating(int id) {
+//        quoteRepository.addRating(id);
+        Optional<Quote> optionalQuote = quoteRepository1.findById(id);
+        if (optionalQuote.isPresent()) {
+            Quote quote = optionalQuote.get();
+            quote.setRating(quote.getRating() + 1);
+            quoteRepository1.save(quote);
+        } else {
+            throw new NoSuchElementException(id + " Quote not present");
+        }
     }
 
     /**
      * Removes 1 rating from the quote
+     *
      * @param id The id of the quote
      */
-    public void removeRating(int id){
-        quoteRepository.removeRating(id);
+    public void removeRating(int id) {
+//        quoteRepository.removeRating(id);
+        Optional<Quote> optionalQuote = quoteRepository1.findById(id);
+        if (optionalQuote.isPresent()) {
+            Quote quote = optionalQuote.get();
+            quote.setRating(quote.getRating() - 1);
+            quoteRepository1.save(quote);
+        } else {
+            throw new NoSuchElementException(id + " Quote not present");
+        }
     }
 
 }
