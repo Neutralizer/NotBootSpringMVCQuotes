d923553e3dc45fad93f8baf09f73c640560146e7
Tsvetan Trifonov <tsmtr_vd@abv.bg>
2020-02-22 13:10:25 -0800

    Finalizing data-jpa

--- src/main/java/com/dao/QuoteRepository.java
+++ src/main/java/com/dao/QuoteRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface QuoteRepository extends PagingAndSortingRepository<Quote,Integer> {
 
 //    List<Quote> findByQuote(String quote);
